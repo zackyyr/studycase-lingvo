1a8d1185627d225b41d265fb9c1ba9d1a9e6f872
Zacky Raechan <muhamadraechanulwanzacky@Jake.local>
2025-02-08 18:08:24 +0800

    hero section done

--- js/script.js
+++ js/script.js
@@ -8,13 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.style.padding = "0.5em 2%";
-        navbar.style.backgroundColor = "rgba(27, 27, 30, 0.9)"; /* Transparan sedikit saat scroll */
-    } else {
-        navbar.style.padding = "1em 2%";
-        navbar.style.backgroundColor = "var(--gluon-grey)";
-    }
-});
